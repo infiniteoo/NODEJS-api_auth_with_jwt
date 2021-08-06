7f776cae24b42d43a8c00771693d60fe6110a4d4
Troy Dorman <troydorman@gmail.com>
2021-08-05 19:22:49 -0600

    created basic server and get route to /api.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API!",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
