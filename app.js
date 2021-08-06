a9935032069baa4460ba98e1f0b6f5d940f8ffd7
Troy Dorman <troydorman@gmail.com>
2021-08-05 19:34:50 -0600

    receiving token back when posting to our api/login route

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.post("/api/posts", (req, res) => {
   });
 });
 
+app.post("/api/login", (req, res) => {
+  // mock user
+  const user = {
+    id: 1,
+    username: "troy",
+    email: "troydorman@gmail.com",
+  };
+
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
 app.listen(5000, () => {
   console.log("server started on port 5000");
 });
